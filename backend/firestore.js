ee98dbe7df789b7d73d82acf068d6feb7bfd06ca
JollyRotten04 <vincejustinebas2004@gmail.com>
2025-10-06 11:34:11 +0800

    Fixed for deployment

--- backend/firestore.js
+++ backend/firestore.js
@@ -1,11 +1,15 @@
 import admin from "firebase-admin";
-import fs from "fs";
 
-const serviceAccount = JSON.parse(fs.readFileSync("./serviceAccountKey.json", "utf8"));
+// Parse the service account from an environment variable
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf-8")
+);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const db = admin.firestore();
 export default db;
